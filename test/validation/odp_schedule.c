77c5137c155b8511c8652d15bcabdefec90173c9
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-04-28 10:43:38 +0200

    * test/validation/: Limit thread count

--- test/validation/odp_schedule.c
+++ test/validation/odp_schedule.c
@@ -682,7 +682,7 @@ static int schd_suite_init(void)
 
 	memset(globals, 0, sizeof(test_globals_t));
 
-	globals->cpu_count = odp_cpu_count();
+	globals->cpu_count = odp_cpu_count() - 1;
 	if (globals->cpu_count > MAX_WORKERS)
 		globals->cpu_count = MAX_WORKERS;
 
