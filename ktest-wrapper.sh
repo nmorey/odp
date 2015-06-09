bdb061d5f4dc880e2b0bfa4cccd697a7c082e4f1
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-06-09 11:36:17 +0200

    * test/Makefile.inc: Always use ktest-wrapper

--- ktest-wrapper.sh
+++ ktest-wrapper.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 ELF=$1
 shift
-k1-jtag-runner --exec-file "Cluster0:$ELF" -- $*
+extension="${ELF##*.}"
+if [ "$extension" != "kelf" ]; then
+	exec ${ELF} $*
+fi
+case "$RUN_TARGET" in
+	"k1-jtag")
+		exec k1-jtag-runner --exec-file "Cluster0:$ELF" -- $*
+		;;
+	"k1-cluster")
+		exec k1-cluster   --functional --dcache-no-check  --mboard=large_memory  --user-syscall=${TOP_SRCDIR}/syscall/build_x86_64/libodp_syscall.so -- $*
+		;;
+	*)
+		exec ${ELF} $*
+esac
