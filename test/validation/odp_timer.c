935a39206a5d0e636e1b72e6274687977b828c34
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-06-24 11:54:01 +0200

    * test/validation/odp_timer.c (my_nanosleep): Remove dependency to nanosleep on K1

--- test/validation/odp_timer.c
+++ test/validation/odp_timer.c
@@ -55,13 +55,18 @@ struct test_timer {
 #define TICK_INVALID (~(uint64_t)0)
 
 static int my_nanosleep(struct timespec *ts){
+#ifdef __K1__
+	uint64_t tdiff = (ts->tv_sec * 400000000ULL) + ((ts->tv_nsec * 400000000ULL) / 1000000000ULL);
 #ifdef MAGIC_SCALL
 	uint64_t divisor = __k1_read_dsu_timestamp_divisor();
 	uint64_t dsu_ts = __k1_read_dsu_timestamp();
 	uint64_t cc = dsu_ts * divisor;
-	uint64_t tdiff = (ts->tv_sec * 400000000ULL) + ((ts->tv_nsec * 400000000ULL) / 1000000000ULL);
 	uint64_t target = cc + tdiff;;
 	return __k1_syscall2(MAGIC_SCALL_SLEEP, target & 0xffffffffULL, target >> 32);
+#else
+	__k1_cpu_backoff(tdiff);
+	return 0;
+#endif
 #else
 	return nanosleep(ts, NULL);
 #endif
