f84e4933888a097032b8e11ecf18c20b50c47193
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-06-24 11:33:19 +0200

    * platform/k1-cluster/os/mos/timer.c: Timer support for mOS

--- platform/k1-cluster/os/mos/timer.c
+++ platform/k1-cluster/os/mos/timer.c
@@ -11,7 +11,8 @@
  *
  */
 
-/* For snprint, POSIX timers and sigevent */
+#include <vbsp.h>
+#include <utask.h>
 
 #include <odp_buffer_inlines.h>
 #include <odp_debug_internal.h>
@@ -20,17 +21,46 @@
 
 
 /******************************************************************************
- * POSIX timer support
- * Functions that use Linux/POSIX per-process timers and related facilities
+ * mOS timer support
+ * Functions that use MPPA per-process timers and related facilities
  *****************************************************************************/
+static odp_timer_pool * _odp_timer_pool_global[BSP_NB_PE_MAX]= { NULL };
 
-
-void _odp_timer_init(odp_timer_pool *tp ODP_UNUSED)
+static void timer_notify (int timer_id ODP_UNUSED)
 {
+	odp_timer_pool * tp = _odp_timer_pool_global[__k1_get_cpu_id()];
+	uint64_t prev_tick = odp_atomic_fetch_inc_u64(&tp->cur_tick);
+	/* Attempt to acquire the lock, check if the old value was clear */
+	if (odp_spinlock_trylock(&tp->itimer_running)) {
+		/* Scan timer array, looking for timers to expire */
+		(void)_odp_timer_pool_expire(tp, prev_tick);
+		odp_spinlock_unlock(&tp->itimer_running);
+	}
+	/* Else skip scan of timers. cur_tick was updated and next itimer
+	 * invocation will process older expiration ticks as well */
+}
+void _odp_timer_init(odp_timer_pool *tp)
+{
+	int pid = __k1_get_cpu_id();
+	if(_odp_timer_pool_global[pid] != NULL){
+		ODP_ABORT("Cannot have more than one timer at once");
+	}
+	_odp_timer_pool_global[pid] = tp;
+	utask_timer64_create(timer_notify);
+
+	uint64_t res  = tp->param.res_ns;
+	uint64_t n_cycles = odp_time_ns_to_cycles(res);
+	utask_timer64_set_time(n_cycles);
 	return;
 }
 
-void _odp_timer_fini(odp_timer_pool *tp ODP_UNUSED)
+void _odp_timer_fini(odp_timer_pool *tp)
 {
+	int pid = __k1_get_cpu_id();
+	/* FIXME: Disable for real */
+	utask_timer64_set_time(1ULL<< 62);
+	if(_odp_timer_pool_global[pid] == tp){
+		_odp_timer_pool_global[pid] = NULL;
+	}
 	return;
 }
