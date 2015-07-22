a3592a083d4f81e960fc725f239d479c7a504a82
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-07-22 20:14:42 +0200

    firmware: rpc: remove debug print
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/rpc/rpc-server.c
+++ firmware/rpc/rpc-server.c
@@ -123,7 +123,6 @@ static int get_if_rx_id(unsigned interface_id)
 		if (bitmask.bitmask[i]) {
 			int rx_id = __k1_ctzdl(bitmask.bitmask[i]) + i * 8 * sizeof(bitmask.bitmask[i]);
 			int ev_counter = mppa_noc_dnoc_rx_lac_event_counter(interface_id, rx_id);
-			printf("%d %d %d has %d events !!\n", interface_id, i, rx_id, ev_counter);
 
 			return rx_id;
 		}
