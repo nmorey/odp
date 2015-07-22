aee768c1a824ebaeb62f82aae270dbb0a5006253
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-07-22 11:10:01 +0200

    firmware: boot: run RPC server at init

--- firmware/boot/ioddr-boot.c
+++ firmware/boot/ioddr-boot.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 #include <mppa_power.h>
 #include <mppa_routing.h>
 #include <mppa_noc.h>
 #include <mppa_bsp.h>
 
-#include <stdio.h>
+#include "odp_rpc_internal.h"
+#include "rpc-server.h"
+
+
+void rpcHandle(unsigned remoteClus, odp_rpc_t *msg, uint8_t *payload)
+{
+
+	(void)remoteClus;
+	(void)payload;
+	odp_rpc_cmd_ack_t ack = {.status = -1 };
+	odp_rpc_server_ack(msg, ack);
+}
 
 int main (int argc, char *argv[])
 {
@@ -13,6 +25,7 @@ int main (int argc, char *argv[])
 	const char *clus_argv[3];
 
 	mppa_power_init();
+	odp_rpc_server_start(rpcHandle);
 
 	clus_count = argc - 1;
 
