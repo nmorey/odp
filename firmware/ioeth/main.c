8c32c3f6a41235bd1452df8b9f58ce084358d00a
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-07-22 10:11:45 +0200

    firmware: rpc: factor RPC server code into a subdirectory for reuse
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/ioeth/main.c
+++ firmware/ioeth/main.c
@@ -4,24 +4,11 @@
 #include <assert.h>
 #include <HAL/hal/hal.h>
 
-#ifndef BSP_NB_DMA_IO_MAX
-#define BSP_NB_DMA_IO_MAX 1
-#endif
-
-#include <mppa_bsp.h>
-#include <mppa_routing.h>
-#include <mppa_noc.h>
-#include "odp_rpc_internal.h"
 #include <odp/plat/atomic_types.h>
+
+#include "odp_rpc_internal.h"
+#include "rpc-server.h"
 #include "eth.h"
-
-#define RPC_PKT_SIZE (sizeof(odp_rpc_t) + RPC_MAX_PAYLOAD)
-
-static struct {
-	uint32_t recv_pkt_count;
-	void    *recv_buf;
-} g_clus_priv[BSP_NB_CLUSTER_MAX];
-
 
 odp_rpc_cmd_ack_t rpcHandle(unsigned remoteClus, odp_rpc_t * msg)
 {
@@ -45,81 +32,26 @@ odp_rpc_cmd_ack_t rpcHandle(unsigned remoteClus, odp_rpc_t * msg)
 	return ack;
 }
 
-static int cluster_init_dnoc_rx(int clus_id)
-{
-	mppa_noc_ret_t ret;
-	mppa_noc_dnoc_rx_configuration_t conf = {0};
-
-	g_clus_priv[clus_id].recv_buf = malloc(RPC_PKT_SIZE);
-	if (!g_clus_priv[clus_id].recv_buf)
-		return 1;
-
-	/* DNoC */
-	ret = mppa_noc_dnoc_rx_alloc(clus_id / 4, RPC_BASE_RX + clus_id % 4);
-	if (ret != MPPA_NOC_RET_SUCCESS)
-		return 1;
-
-	conf.buffer_base = (uintptr_t)g_clus_priv[clus_id].recv_buf;
-	conf.buffer_size = RPC_PKT_SIZE;
-	conf.activation = MPPA_NOC_ACTIVATED;
-	conf.reload_mode = MPPA_NOC_RX_RELOAD_MODE_INCR_DATA_NOTIF;
-
-	ret = mppa_noc_dnoc_rx_configure(clus_id / 4, RPC_BASE_RX + clus_id % 4, conf);
-	if (ret != MPPA_NOC_RET_SUCCESS)
-		return 1;
-
-	return 0;
-}
-
-static int get_if_rx_id(unsigned interface_id)
-{
-	mppa_noc_dnoc_rx_bitmask_t bitmask = mppa_noc_dnoc_rx_get_events_bitmask(interface_id);
-	for (int i = 0; i < 3; ++i) {
-		if (bitmask.bitmask[i]) {
-			int rx_id = __k1_ctzdl(bitmask.bitmask[i]) + i * 8 * sizeof(bitmask.bitmask[i]);
-			mppa_noc_dnoc_rx_lac_event_counter(interface_id, rx_id);
-
-			return rx_id;
-		}
-	}
-	return -1;
-}
-
-static int ack_msg(odp_rpc_t * msg, odp_rpc_cmd_ack_t ack)
-{
-	msg->ack = 1;
-	msg->data_len = 0;
-	msg->inl_data = ack.inl_data;
-
-	return odp_rpc_send_msg(msg->dma_id / 4, msg->dma_id, msg->dnoc_tag, msg, NULL);
-}
-
 int main()
 {
+
 	int ret;
 
-	for (int i = 0; i < BSP_NB_CLUSTER_MAX; ++i) {
-		ret = cluster_init_dnoc_rx(i);
-		if (ret != 0) {
-			exit(EXIT_FAILURE);
-		}
-	}
 	eth_init();
 
+	ret = odp_rpc_server_start(NULL);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+
 	while (1) {
-		for (int if_id = 0; if_id < 4; ++if_id) {
-			int tag = get_if_rx_id(if_id);
-			if(tag < 0)
-				continue;
+		int remoteClus;
+		odp_rpc_t *msg;
 
-			/* Received a message */
-			int remoteClus = 4 * if_id + (tag - RPC_BASE_RX);
-			odp_rpc_t *msg = g_clus_priv[remoteClus].recv_buf;
-
+		remoteClus = odp_rpc_server_poll_msg(&msg, NULL);
+		if(remoteClus >= 0) {
 			odp_rpc_cmd_ack_t ack = rpcHandle(remoteClus, msg);
-
-			/* Ack the RPC message */
-			ack_msg(msg, ack);
+			odp_rpc_server_ack(msg, ack);
 		}
 	}
 	return 0;
---
+++ firmware/rpc/rpc-server.h
@@ -0,0 +1,10 @@
+#ifndef RPC_SERVER__H
+#define RPC_SERVER__H
+
+typedef void (*odp_rpc_handler_t)(unsigned remoteClus, odp_rpc_t * msg);
+
+int odp_rpc_server_start(odp_rpc_handler_t handler);
+int odp_rpc_server_poll_msg(odp_rpc_t **msg, uint8_t **payload);
+int odp_rpc_server_ack(odp_rpc_t * msg, odp_rpc_cmd_ack_t ack);
+
+#endif /* RPC_SERVER__H */
