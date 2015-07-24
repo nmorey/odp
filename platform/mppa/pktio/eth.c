bc63078635ec61a2ac4e5355544508dae4d54a58
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-07-24 11:01:44 +0200

    mppa: pktio/eth: implement rx support
     Also move struct to file and use malloc so we don't get a hude pool_entry union
    
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- platform/mppa/pktio/eth.c
+++ platform/mppa/pktio/eth.c
@@ -16,15 +16,81 @@
 
 #include <mppa_noc.h>
 
+static inline int MIN(int a, int b)
+{
+	return a > b ? b : a;
+}
+
+static inline int MAX(int a, int b)
+{
+	return b > a ? b : a;
+}
+
 static int eth_init(void)
 {
 	return 0;
 }
 
+typedef struct eth_status {
+	odp_pool_t pool; 		 /**< pool to alloc packets from */
+	uint8_t slot_id;                 /**< IO Eth Id */
+	uint8_t port_id;                 /**< Eth Port id. 4 for 40G */
+	uint8_t dma_if;                  /**< DMA Rx Interface */
+	uint8_t min_port;                /**< Minimum port in the port range */
+	uint8_t max_port;                /**< Maximum port in the port range */
+	odp_queue_t queue;		 /**< Internal queue to store packets  */
+	unsigned long long ev_masks[4];  /**< Mask to isolate events that belong to us */
+	odp_packet_t pkts[N_RX_PER_PORT];/**< Pointer to PKT mapped to Rx tags */
+	uint64_t dropped_pkts;           /**< Number of droppes pkts */
+	uint8_t refresh_rx;
+} eth_status_t;
+
+static int _eth_configure_rx(eth_status_t *eth, int rxId)
+{
+	odp_packet_t pkt = _odp_packet_alloc(eth->pool);
+	if (pkt == ODP_PACKET_INVALID)
+		return -1;
+
+	mppa_noc_dnoc_rx_configuration_t conf = {
+		.buffer_base = (unsigned long)odp_packet_data(pkt),
+		.buffer_size = odp_packet_len(pkt),
+		.current_offset = 0,
+		.item_counter = 1,
+		.item_reload = 1,
+		.reload_mode = MPPA_NOC_RX_RELOAD_MODE_DECR_NOTIF_NO_RELOAD_IDLE,
+		.activation = MPPA_NOC_ACTIVATED,
+		.counter_id = 0
+	};
+	eth->pkts[rxId - eth->min_port] = pkt;
+	return mppa_noc_dnoc_rx_configure(eth->dma_if, rxId, conf);
+}
+
+static odp_packet_t _eth_reload_rx(eth_status_t *eth, int rxId)
+{
+	int rank = rxId - eth->min_port;
+	odp_packet_t pkt = eth->pkts[rank];
+
+	odp_packet_t new_pkt = _odp_packet_alloc(eth->pool);
+	if (pkt == ODP_PACKET_INVALID) {
+		eth->refresh_rx = 1;
+		return pkt;
+	}
+
+	mppa_dnoc[eth->dma_if]->rx_queues[rxId].buffer_base.dword = (unsigned long)odp_packet_data(new_pkt);
+	eth->pkts[rank] = new_pkt;
+
+	eth->dropped_pkts += mppa_dnoc[eth->dma_if]->rx_queues[rxId].get_drop_pkt_nb_and_activate.reg;
+
+	return pkt;
+}
 
 static int eth_open(odp_pktio_t id ODP_UNUSED, pktio_entry_t *pktio_entry,
 		    const char *devname, odp_pool_t pool)
 {
+
+	/*
+	 * Check device name and extract slot/port
+	 */
 	if (devname[0] != 'p')
 		return -1;
 
@@ -45,23 +111,35 @@ static int eth_open(odp_pktio_t id ODP_UNUSED, pktio_entry_t *pktio_entry,
 			return -1;
 	}
 
-	pktio_entry->s.pkt_eth.slot_id = slot_id;
-	pktio_entry->s.pkt_eth.port_id = port_id;
-	pktio_entry->s.pkt_eth.pool = pool;
+	pktio_entry->s.pkt_eth.status = malloc(sizeof(*pktio_entry->s.pkt_eth.status));
+	if (!pktio_entry->s.pkt_eth.status)
+		return -1;
 
+	eth_status_t * eth = pktio_entry->s.pkt_eth.status;
+	/*
+	 * Init eth status
+	 */
+	eth->pool = pool;
+	eth->slot_id = slot_id;
+	eth->port_id = port_id;
+	eth->dma_if = 0;
+
+	/*
+	 * Allocate contiguous RX ports
+	 */
 	int first_rx = -1;
 	int n_rx;
 	for (n_rx = 0; n_rx < N_RX_PER_PORT; ++n_rx) {
 		mppa_noc_ret_t ret;
 		unsigned rx_port;
 
-		ret = mppa_noc_dnoc_rx_alloc_auto(0, &rx_port, MPPA_NOC_BLOCKING);
+		ret = mppa_noc_dnoc_rx_alloc_auto(eth->dma_if, &rx_port, MPPA_NOC_BLOCKING);
 		if(ret != MPPA_NOC_RET_SUCCESS)
 			break;
 
 		if (first_rx >= 0 && (unsigned)(first_rx + n_rx) != rx_port) {
 			/* Non contiguous port... Fail */
-			mppa_noc_dnoc_rx_free(0, rx_port);
+			mppa_noc_dnoc_rx_free(eth->dma_if, rx_port);
 			break;
 		}
 		if(first_rx < 0)
@@ -75,13 +153,61 @@ static int eth_open(odp_pktio_t id ODP_UNUSED, pktio_entry_t *pktio_entry,
 		n_rx --;
 
 		for ( ;n_rx >= 0; --n_rx)
-			mppa_noc_dnoc_rx_free(0, first_rx + n_rx);
+			mppa_noc_dnoc_rx_free(eth->dma_if, first_rx + n_rx);
 	}
+
+	eth->min_port = first_rx;
+	eth->max_port = first_rx + n_rx - 1;
+
+	/*
+	 * Compute event mask to detect events on our own tags later
+	 */
+	unsigned long long full_mask = (unsigned long long)(-1LL);
+
+	for(int i = 0; i < 4; ++i){
+		if (eth->min_port >= (i + 1) * 64 || eth->max_port < i * 64) {
+			eth->ev_masks[i] = 0ULL;
+			continue;
+		}
+		uint8_t local_min = MAX(i * 64, eth->min_port) - (i * 64);
+		uint8_t local_max = MIN((i + 1) * 64 - 1, eth->max_port) - (i * 64);
+		eth->ev_masks[i] = 
+			(/* Trim the upper bits */
+			 (
+			  /* Trim the lower bits */
+			  full_mask >> (local_min)
+			  )
+			 /* Realign back + trim the top */
+			 << (local_min + 63 - local_max)
+			 ) /* Realign again */ >> (63 - local_max);
+	}
+
+	for (int i = eth->min_port; i < eth->max_port; ++i) {
+		_eth_configure_rx(eth, i);
+	}
+
+	/*
+	 * Internal queue to store packet faster than recv may ask us
+	 * so we can keep the RX open
+	 */
+	char q_name[ODP_QUEUE_NAME_LEN];
+	snprintf(q_name, sizeof(q_name), "%" PRIu64 "-pktio_internalq",
+		 odp_pktio_to_u64(id));
+	eth->queue =
+		odp_queue_create(q_name, ODP_QUEUE_TYPE_POLL, NULL);
+
+	if (eth->queue == ODP_QUEUE_INVALID)
+		return -1;
+
+	/*
+	 * RPC Msg to IOETH  #N so the LB will dispatch to us
+	 */
 	odp_rpc_cmd_open_t open_cmd = {
 		{
 			.ifId = port_id,
-			.min_rx = first_rx,
-			.max_rx = first_rx + n_rx - 1
+			.dma_if = eth->dma_if,
+			.min_rx = eth->min_port,
+			.max_rx = eth->max_port,
 		}
 	};
 	unsigned cluster_id = __k1_get_cluster_id();
@@ -104,12 +230,14 @@ static int eth_open(odp_pktio_t id ODP_UNUSED, pktio_entry_t *pktio_entry,
 static int eth_close(pktio_entry_t * const pktio_entry)
 {
 
-	int slot_id = pktio_entry->s.pkt_eth.slot_id;
-	int port_id = pktio_entry->s.pkt_eth.port_id;
+	eth_status_t * eth = pktio_entry->s.pkt_eth.status;
+	int slot_id = eth->slot_id;
+	int port_id = eth->port_id;
 
+	odp_queue_destroy(eth->queue);
 	odp_rpc_cmd_clos_t close_cmd = {
 		{
-			.ifId = pktio_entry->s.pkt_eth.port_id = port_id
+			.ifId = eth->port_id = port_id
 
 		}
 	};
@@ -127,6 +255,9 @@ static int eth_close(pktio_entry_t * const pktio_entry)
 	odp_rpc_t * ack_msg;
 	odp_rpc_wait_ack(&ack_msg, NULL);
 	odp_rpc_cmd_ack_t ack = { .inl_data = ack_msg->inl_data};
+
+	free(eth);
+	pktio_entry->s.pkt_eth.status = NULL;
 	return ack.status;
 }
 
@@ -142,7 +273,39 @@ static int eth_recv(pktio_entry_t *pktio_entry ODP_UNUSED,
 		    unsigned len ODP_UNUSED)
 {
 
-	return 0;
+	eth_status_t * eth = pktio_entry->s.pkt_eth.status;
+	mppa_noc_dnoc_rx_bitmask_t bitmask = mppa_noc_dnoc_rx_get_events_bitmask(eth->dma_if);
+	unsigned nb_rx = 0;
+
+	if (eth->dropped_pkts) {
+		eth->dropped_pkts = 0;
+		for (int i = 0; i < N_RX_PER_PORT; ++i)
+			_eth_reload_rx(eth, eth->min_port + i);
+	}
+
+	for (int i = 0; i < 3 && nb_rx < len; ++i) {
+		uint64_t mask = eth->ev_masks[i] & bitmask.bitmask[i];
+		if (mask == 0ULL)
+			continue;
+
+		/* We have an event */
+		while (mask != 0ULL && nb_rx < len) {
+			int mask_bit = __k1_ctzdl(mask);
+			int rx_id = mask_bit + i * 64;
+			int ev_counter = mppa_noc_dnoc_rx_lac_event_counter(eth->dma_if, rx_id);
+			assert(ev_counter > 0);
+
+			odp_packet_t pkt = _eth_reload_rx(eth, rx_id);
+			/* Parse and set packet header data */
+			/* FIXME: odp_packet_pull_tail(pkt, pkt_sock->max_frame_len - recv_bytes); */
+			_odp_packet_reset_parse(pkt);
+
+			pkt_table[nb_rx++] = pkt;
+			mask = mask ^ (1ULL << mask_bit);
+
+		}
+	}
+	return nb_rx;
 }
 
 static int eth_send(pktio_entry_t *pktio_entry ODP_UNUSED,
