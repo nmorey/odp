223ffaa3099f87e03602068063b98e8fb5946bf3
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-09-17 14:56:56 +0200

    mppa: rpc: add static initializer for ack message
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- firmware/common/pcie/mppa_pcie_noc.c
+++ firmware/common/pcie/mppa_pcie_noc.c
@@ -54,7 +54,7 @@ int mppa_pcie_eth_setup_tx(unsigned int iface_id, unsigned int tx_id, unsigned i
 odp_rpc_cmd_ack_t mppa_pcie_eth_open(unsigned remoteClus, odp_rpc_t * msg)
 {
 	odp_rpc_cmd_pcie_open_t open_cmd = {.inl_data = msg->inl_data};
-	odp_rpc_cmd_ack_t ack = {.status = -1};
+	odp_rpc_cmd_ack_t ack = ODP_RPC_CMD_ACK_INITIALIZER;
 	int if_id = remoteClus % BSP_NB_DMA_IO_MAX;
 	int tx_id = remoteClus / BSP_NB_DMA_IO_MAX;
 	unsigned int rx_id;
