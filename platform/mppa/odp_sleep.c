7f6d7b60b38a29682df2ade355d451724f3c40fa
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-09-22 09:54:21 +0200

    mppa: sleep: add sleep implementation
    * Fixes broken sleep in nodeos
    * Redirect all sleep to magic syscall in simu mode for faster simulations
    
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ platform/mppa/odp_sleep.c
@@ -0,0 +1,43 @@
+/* Copyright (c) 2015, Kalray
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP sleep service
+ *
+ */
+
+#include <stdint.h>
+#include <unistd.h>
+
+static int my_nanosleep(struct timespec *ts){
+	uint64_t freq = __bsp_frequency;
+	uint64_t tdiff = (ts->tv_sec * freq) + ((ts->tv_nsec * freq) / 1000000000ULL);
+
+#ifdef MAGIC_SCALL
+ 	uint64_t divisor = __k1_read_dsu_timestamp_divisor();
+	uint64_t dsu_ts = __k1_read_dsu_timestamp();
+	uint64_t cc = dsu_ts * divisor;
+	uint64_t target = cc + tdiff;;
+	return __k1_syscall2(MAGIC_SCALL_SLEEP, target & 0xffffffffULL, target >> 32);
+#else
+	while(tdiff > UINT32_MAX){
+		__k1_cpu_backoff(UINT32_MAX);
+		tdiff -= UINT32_MAX;
+	}
+	__k1_cpu_backoff(tdiff);
+	return 0;
+#endif
+}
+
+unsigned int sleep(unsigned int seconds){
+   struct timespec ts;
+
+    ts.tv_sec = seconds;
+    ts.tv_nsec = 0;
+    return my_nanosleep(&ts);
+};
