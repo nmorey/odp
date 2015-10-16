6cc3756c0d16870b289d6a943ce26d8ceb3e2624
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-10-16 11:30:15 +0200

    ktest-wrapper: auto load firmware on HW
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- ktest-wrapper.sh
+++ ktest-wrapper.sh
@@ -5,13 +5,33 @@ extension="${ELF##*.}"
 if [ "$extension" != "kelf" ]; then
 	exec ${ELF} $*
 fi
+
 case "$RUN_TARGET" in
 	"k1-jtag")
-		exec k1-jtag-runner --exec-file "Cluster0:$ELF" -- $*
+		BOARD_TYPE=$(cat /mppa/board0/type)
+		FIRMWARES=""
+		case "$BOARD_TYPE" in
+			"ab01")
+				FIRMWARES="--exec-file IODDR0:${TOP_BUILDDIR}/../iounified/k1b-kalray-iounified/iounified.kelf --exec-file IODDR1:${TOP_BUILDDIR}/../iounified/k1b-kalray-iounified/iounified.kelf "
+				;;
+			"explorer")
+				FIRMWARES="--exec-file IOETH1:${TOP_BUILDDIR}/../ioeth/k1b-kalray-ioeth530/ioeth.kelf"
+				;;
+			"")
+				;;
+			*)
+				;;
+		esac
+
+		echo k1-jtag-runner ${FIRMWARES} --exec-file "Cluster0:$ELF" -- $*
+		exec k1-jtag-runner ${FIRMWARES} --exec-file "Cluster0:$ELF" -- $*
 		;;
 	"k1-cluster")
+		echo k1-cluster   --functional --dcache-no-check  --mboard=large_memory  --user-syscall=${TOP_SRCDIR}/syscall/build_x86_64/libodp_syscall.so -- $ELF $*
 		exec k1-cluster   --functional --dcache-no-check  --mboard=large_memory  --user-syscall=${TOP_SRCDIR}/syscall/build_x86_64/libodp_syscall.so -- $ELF $*
 		;;
 	*)
+		echo ${ELF} $*
 		exec ${ELF} $*
 esac
+
