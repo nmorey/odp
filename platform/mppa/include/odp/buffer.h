d7b1950dcf3e738037c9d62f92b1e3eec0d71d70
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-11-17 10:35:32 +0100

    mppa: buffer: move event <=> buffer functions to header
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- platform/mppa/include/odp/buffer.h
+++ platform/mppa/include/odp/buffer.h
@@ -1,1 +1,48 @@
-../../../linux-generic/include/odp/buffer.h
+/* Copyright (c) 2013, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP buffer descriptor
+ */
+
+#ifndef ODP_PLAT_BUFFER_H_
+#define ODP_PLAT_BUFFER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/std_types.h>
+#include <odp/plat/event_types.h>
+#include <odp/plat/buffer_types.h>
+#include <odp/plat/pool_types.h>
+
+/** @ingroup odp_buffer
+ *  @{
+ */
+
+static inline odp_buffer_t odp_buffer_from_event(odp_event_t ev)
+{
+	return (odp_buffer_t)ev;
+}
+
+static inline odp_event_t odp_buffer_to_event(odp_buffer_t buf)
+{
+	return (odp_event_t)buf;
+}
+/**
+ * @}
+ */
+
+#include <odp/api/buffer.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
